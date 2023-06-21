d558c941d1b65b6580c0aa69b3466d646823ef98
unknown <nishanneupane368@gmail.com>
2023-06-21 19:45:23 +0545

    Kadam project

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/action-g7c6a1959b_640.jpg';
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#b0de1b" fontWeight="600" fontSize="26px">Kadam Fitness</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Sweat, Strive <br />
+      Flex, Flourish <br />
+      And Repeat
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#b0de1b', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#b0de1b" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Work Hard
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" style={{
+      height:"90%",
+      borderRadius:"50%",
+      opacity:"0.7"
+    }}/>
+  </Box>
+);
+
+export default HeroBanner;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '28b2812a36mshd8c2b7256e5bec7p142682jsnf16bd9f57fe8',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
